57cd7c85a5df10ad660ae65a3bedb852d79974b0
Ivan <sampad777@gmail.com>
2018-12-15 13:38:23 +0300

    Это наш второй комит

---
+++ project2/index8.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8" />
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="bayzel" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+	<title>Лаврик 5 урок ajax</title>
+</head>
+
+<body>
+
+    <div id="test">this is the labrary</div>
+    <div id="raz">a</div>
+
+    
+
+    <ul>
+        <li id="one">раз</li>
+        <li id="two">два</li>
+        <li id="three">три</li>
+        <li id="four">четыре</li>
+    </ul>
+
+    <script>
+        'use strict'
+        var testVar = "my name is Sergey";
+
+        
+        testUser = prompt("Введите число", "");
+        console.log(testUser);
+        console.log(testUser/2);
+        console.log(testUser/3);
+
+    </script>
+
+</body>
+</html>
