0a2e7d9d1e03f0c10159d86015adec400a30f8d3
Ivan <sampad777@gmail.com>
2018-12-15 13:00:23 +0300

    Это наш первый комит

---
+++ index5.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="bayzel" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+	<title>Лаврик 5 урок ajax</title>
+</head>
+
+<body>
+
+    <form>
+        <input type="text" name="login" value="login" />
+        <input type="password" name="pass" value="pass"/>
+        <br /><br />
+        <input type="button" id="btnAjax" value="AJAX" />
+        <input type="button" id="btnLoad" value="Load" />
+        <input type="button" id="btnGet" value="GET" />
+        <input type="button" id="btnPost" value="POST" />
+    
+    </form>
+
+</body>
+</html>
---
+++ index6.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="bayzel" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+	<title>Лаврик 6 урок ajax</title>
+</head>
+
+<body>
+
+   <script>
+   
+        $Data = {
+            "name": "Ваня",
+            "old": "40"
+        }
+   
+   
+   </script>
+
+</body>
+</html>
